a8eaea136c43b2970e16e4ecfb067c67e251a390
Xinbo <xinboliu555@gamil.com>
2025-01-07 14:50:43 +0100

    Este es mi segundo commit

---
+++ holagit2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class holagit{
+    public static void main(String[]args){
+        System.out.println("Holagit2");
+        System.out.println("Hey, que tal estas?");
+    }
+}
