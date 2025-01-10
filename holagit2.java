fedbe00b11e6ae86bf39c0c43f826246b260e8b8
Xinbo <xinboliu555@gamil.com>
2025-01-10 20:33:28 +0100

    Pongo todo en su lugar correcto

--- holagit2.java
+++ holagit2.java
@@ -1,9 +1,9 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class holagit{
+public class holagit2{
     public static void main(String[]args){
-        System.out.println("Holagit2");
+        System.out.println("Holagit2 con cambios");
         System.out.println("Hey, que tal estas?");
     }
 }
