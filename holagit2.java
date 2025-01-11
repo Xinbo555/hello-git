8e95eef96dd6b4fa66680905991c623611db0a31
Xinbo <xinboliu555@gamil.com>
2025-01-11 13:42:12 +0100

    ensaje eliminado

--- holagit2.java
+++ holagit2.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 public class holagit2{
     public static void main(String[]args){
         System.out.println("Holagit2 con cambios");
-        System.out.println("Hey, que tal estas?");
+ //mensaje eliminado 
     }
 }
