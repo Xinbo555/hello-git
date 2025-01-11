3ef8818c23ad231bf3c05439a5b80477d94a9764
Xinbo <xinboliu555@gamil.com>
2025-01-11 14:13:04 +0100

    version 3 del login

--- login.java
+++ login.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 
 public class holagit2{
     public static void main(String[]args){
-        System.out.println("LOGINv2");
+        System.out.println("LOGINv3");
     }
 }
