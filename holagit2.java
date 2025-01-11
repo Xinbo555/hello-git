6cb578e89a4f56d15380945df92cb380f07c42da
Xinbo <xinboliu555@gamil.com>
2025-01-11 13:48:00 +0100

    git 2 login

--- holagit2.java
+++ holagit2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class holagit2{
     public static void main(String[]args){
-        System.out.println("Holagit2 con cambios");
+        System.out.println("Holagit2 vlogin");
  //mensaje eliminado 
     }
 }
